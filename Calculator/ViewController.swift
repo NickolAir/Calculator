f4227c23571eab45c0187a38090a7ac9ba2c864f
nikolayratushnyak <nikolay.ratushnyak03@gmail.com>
2024-10-13 23:31:21 +0700

    added viewModel class and fixed some bugs

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,21 +14,25 @@ class ViewController: UIViewController {
         case lastSymbolOperator
         case invalidExpression
     }
+    
+    private let viewModel = ViewModel()
+    private var isError = false
 
     @IBAction func ACButtonAction(_ sender: Any) {
-        animateButton(button: acOutlet)
+        viewModel.animateButton(button: acOutlet)
+        isError = false
         inputFieldOutlet.text? = ""
     }
     
     @IBAction func PlusMinusAction(_ sender: Any) {
-        animateButton(button: plusMinusOutlet)
+        viewModel.animateButton(button: plusMinusOutlet)
         if let inputField = inputFieldOutlet.text {
             if (inputField.count != 0) {
                 if (inputField.hasPrefix("-(")) {
                     inputFieldOutlet.text?.removeFirst()
-                    removeBrackets(field: inputFieldOutlet)
+                    viewModel.removeBrackets(field: inputFieldOutlet)
                 } else {
-                    addBrackets(field: inputFieldOutlet)
+                    viewModel.addBrackets(field: inputFieldOutlet)
                     inputFieldOutlet.text?.insert("-", at: inputField.startIndex)
                 }
             }
@@ -36,72 +40,72 @@ class ViewController: UIViewController {
     }
     
     @IBAction func percentAction(_ sender: Any) {
-        animateButton(button: percentOutlet)
+        viewModel.animateButton(button: percentOutlet)
         if (inputFieldOutlet.text?.count != 0) {
-            addBrackets(field: inputFieldOutlet)
+            viewModel.addBrackets(field: inputFieldOutlet)
             inputFieldOutlet.text?.append("*0.01")
         }
     }
     
     @IBAction func oneAction(_ sender: Any) {
-        animateButton(button: oneOutlet)
+        viewModel.animateButton(button: oneOutlet)
         inputFieldOutlet.text?.append("1")
     }
     @IBAction func twoAction(_ sender: Any) {
-        animateButton(button: twoOutlet)
+        viewModel.animateButton(button: twoOutlet)
         inputFieldOutlet.text?.append("2")
     }
     @IBAction func fiveAction(_ sender: Any) {
-        animateButton(button: fiveOutlet)
+        viewModel.animateButton(button: fiveOutlet)
         inputFieldOutlet.text?.append("5")
     }
     @IBAction func sixAction(_ sender: Any) {
-        animateButton(button: sixOutlet)
+        viewModel.animateButton(button: sixOutlet)
         inputFieldOutlet.text?.append("6")
     }
     @IBAction func divideAction(_ sender: Any) {
-        animateButton(button: divideOutlet)
-        if (!isLastOperator(field: inputFieldOutlet) && inputFieldOutlet.text?.count != 0) {
+        viewModel.animateButton(button: divideOutlet)
+        if (!viewModel.isLastOperator(field: inputFieldOutlet) && inputFieldOutlet.text?.count != 0) {
             inputFieldOutlet.text?.append("/")
         }
     }
     @IBAction func eightAction(_ sender: Any) {
-        animateButton(button: eightOutlet)
+        viewModel.animateButton(button: eightOutlet)
         inputFieldOutlet.text?.append("8")
     }
     @IBAction func nineAction(_ sender: Any) {
-        animateButton(button: nineOutlet)
+        viewModel.animateButton(button: nineOutlet)
         inputFieldOutlet.text?.append("9")
     }
     @IBAction func deleteAction(_ sender: Any) {
-        animateButton(button: deleteOutlet)
+        viewModel.animateButton(button: deleteOutlet)
         if (inputFieldOutlet.text?.count != 0) {
             inputFieldOutlet.text?.removeLast()
         }
     }
     @IBAction func sevenAction(_ sender: Any) {
-        animateButton(button: sevenOutlet)
+        viewModel.animateButton(button: sevenOutlet)
         inputFieldOutlet.text?.append("7")
     }
     @IBAction func fourAction(_ sender: Any) {
-        animateButton(button: fourOutlet)
+        viewModel.animateButton(button: fourOutlet)
         inputFieldOutlet.text?.append("4")
     }
     @IBAction func minusAction(_ sender: Any) {
-        animateButton(button: minusOutlet)
-        if (!isLastOperator(field: inputFieldOutlet)) {
+        viewModel.animateButton(button: minusOutlet)
+        if (!viewModel.isLastOperator(field: inputFieldOutlet)) {
             inputFieldOutlet.text?.append("-")
         }
     }
     @IBAction func multiplyAction(_ sender: Any) {
-        animateButton(button: multiplyOutlet)
-        if (!isLastOperator(field: inputFieldOutlet) && inputFieldOutlet.text?.count != 0) {
+        viewModel.animateButton(button: multiplyOutlet)
+        if (!viewModel.isLastOperator(field: inputFieldOutlet) && inputFieldOutlet.text?.count != 0) {
             inputFieldOutlet.text?.append("*")
         }
     }
     @IBAction func equalAction(_ sender: Any) {
-        animateButton(button: equalOutlet)
-            
+        viewModel.animateButton(button: equalOutlet)
+        if(!isError) {
             do {
                 try validateInput()
                 
@@ -115,32 +119,41 @@ class ViewController: UIViewController {
                 }
             } catch CalculationError.lastSymbolDot {
                 inputFieldOutlet.text = "'.' can't be last symbol"
+                isError = true
             } catch CalculationError.lastSymbolOperator {
                 inputFieldOutlet.text = "Operator can't be last"
+                isError = true
             } catch CalculationError.invalidExpression {
                 inputFieldOutlet.text = "Incorrect input"
+                isError = true
             } catch {
                 inputFieldOutlet.text = "Unknown error"
+                isError = true
             }
         }
+    }
     @IBAction func plusAction(_ sender: Any) {
-        animateButton(button: plusOutlet)
-        if (!isLastOperator(field: inputFieldOutlet)) {
+        viewModel.animateButton(button: plusOutlet)
+        if (!viewModel.isLastOperator(field: inputFieldOutlet)) {
             inputFieldOutlet.text?.append("+")
         }
     }
     @IBAction func commaAction(_ sender: Any) {
-        animateButton(button: commaOutlet)
-        if (!isLastDot(field: inputFieldOutlet) && inputFieldOutlet.text?.count != 0) {
+        viewModel.animateButton(button: commaOutlet)
+        if (!viewModel.isLastDot(field: inputFieldOutlet) && inputFieldOutlet.text?.count != 0){
+            let components = inputFieldOutlet.text?.split(whereSeparator: { "+-*/".contains($0) })
+            if let lastComponent = components?.last, lastComponent.contains(".") {
+                return
+            }
             inputFieldOutlet.text?.append(".")
         }
     }
     @IBAction func zeroAction(_ sender: Any) {
-        animateButton(button: zeroOutlet)
+        viewModel.animateButton(button: zeroOutlet)
         inputFieldOutlet.text?.append("0")
     }
     @IBAction func threeAction(_ sender: Any) {
-        animateButton(button: threeOutlet)
+        viewModel.animateButton(button: threeOutlet)
         inputFieldOutlet.text?.append("3")
     }
     
@@ -166,6 +179,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var divideOutlet: UIButton!
     @IBOutlet weak var percentOutlet: UIButton!
     
+    func validateInput() throws {
+        if viewModel.isLastDot(field: inputFieldOutlet) {
+            throw CalculationError.lastSymbolDot
+        }
+        
+        if viewModel.isLastOperator(field: inputFieldOutlet) {
+            throw CalculationError.lastSymbolOperator
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+class ViewModel {
     func animateButton(button: UIButton) {
         UIView.animate(withDuration: 0.1, animations: {
                 button.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
@@ -208,21 +239,5 @@ class ViewController: UIViewController {
             }
         }
         return false
-    }
-    
-    func validateInput() throws {
-        if isLastDot(field: inputFieldOutlet) {
-            throw CalculationError.lastSymbolDot
-        }
-        
-        if isLastOperator(field: inputFieldOutlet) {
-            throw CalculationError.lastSymbolOperator
-        }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
     }
 }
